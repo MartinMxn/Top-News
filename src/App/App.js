c86818cdf9c4f97bfb35f14e1e043e3b0794c2f9
MartinMxn <meng.xua@husky.neu.edu>
2019-07-01 23:18:27 +0800

    init

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import 'materialize-css';
+
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img className="logo" src={logo} alt="logo"/>
+      <div className="container"></div>
+    </div>
+  );
+}
+
+export default App;
