ae37b3908c4f75cbf004360080abe97e75c19a25
MartinMxn <meng.xua@husky.neu.edu>
2019-07-06 21:11:48 +0800

    front end route & auth & login & logout

--- web_server/client/src/index.js
+++ web_server/client/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { browserHistory, Router } from "react-router";
+import routes from "./routes";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "materialize-css/dist/css/materialize.min.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById("root")
+);
